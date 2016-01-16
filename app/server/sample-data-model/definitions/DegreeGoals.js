3d47388d6919c2b2c43a4f559d1688ff98a5a1b7
Gilbert Yim <gyim@hawaii.edu>
2016-01-16 00:39:44 -1000

    Added new degree goals

--- app/server/sample-data-model/definitions/DegreeGoals.js
+++ app/server/sample-data-model/definitions/DegreeGoals.js
@@ -2,8 +2,9 @@ defineDegreeGoals = function() {
   let sampleDegreeGoals = [
     {name: "Silicon Valley Tech", slug: "degree-goal-sv-tech", description: "TBD"},
     {name: "Ph.D. Prep", slug: "degree-goal-phd-prep", description: "TBD"},
-    {name: "security", slug: "degree-goal-security", description: "TBD"}
-
+    {name: "security", slug: "degree-goal-security", description: "TBD"},
+    {name: "Network Engineer", slug: "degree-goal-network-engineer", description: "The field concerned with internetworking service requirements for switched telephone networks and developing the required hardware and software."},
+    {name: "Video Game Developer", slug: "degree-goal-game-dev", description: "A software developer that specializes in video game development, the process and related disciplines of creating a video game."}
   ];
 
   _.each(sampleDegreeGoals, RadGrad.degreegoal.define);
